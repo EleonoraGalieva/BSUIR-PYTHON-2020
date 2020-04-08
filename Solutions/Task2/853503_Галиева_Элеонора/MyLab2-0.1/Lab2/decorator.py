876a4e7aa8162d5e6eeac7a97c9326f4eed4cdcb
EleonoraGalieva <48092813+EleonoraGalieva@users.noreply.github.com>
2020-04-08 23:39:19 +0300

    Update decorator.py

--- Solutions/Task2/853503_Галиева_Элеонора/MyLab2-0.1/Lab2/decorator.py
+++ Solutions/Task2/853503_Галиева_Элеонора/MyLab2-0.1/Lab2/decorator.py
@@ -1,20 +1,17 @@
-import collections
-
-
 class Memoized:
     def __init__(self, func, cache_size=8):
         self.func = func
         self.cache = {}
         self.cache_size = cache_size
 
-    def __call__(self, args):
-        if isinstance(args, collections.Hashable):
-            if args in self.cache:
-                return self.cache[args]
+    def __call__(self, *args):
+        if isinstance(args[0], (int, str, tuple)):
+            if args[0] in self.cache:
+                return self.cache[args[0]]
             else:
-                value = self.func(args)
+                value = self.func(*args)
                 if len(self.cache) < self.cache_size:
-                    self.cache[args] = value
+                    self.cache[args[0]] = value
                 return value
         else:
-            return self.func(args)
+            return self.func(*args)
